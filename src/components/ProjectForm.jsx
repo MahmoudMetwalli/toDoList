0044283ccc9744a665d3464482382d4d6828945f
Mahmoud Metwalli <mahmoud.metwaly@vconnct.com>
2025-11-06 14:35:07 +0200

    Project Form

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,21 +1,31 @@
 export default function ProjectForm({ onSubmit }) {
   return (
-    <form onSubmit={onSubmit} className="bg-yellow-500">
-      <label htmlFor="projectName" className="text-white">
-        Project Name
-      </label>
-      <input type="text" id="projectName" name="projectName" />
-      <label htmlFor="projectDescription" className="text-white">
-        Project Description
-      </label>
-      <input type="text" id="projectDescription" name="projectDescription" />
-      <label htmlFor="projectDueDate" className="text-white">
-        Project Due Date
-      </label>
-      <input type="date" id="projectDueDate" name="projectDueDate" />
-      <button type="submit" className="text-white">
-        Save
-      </button>
+    <form onSubmit={onSubmit} className="">
+      <div>
+        <button
+          type="submit"
+          className="text-white bg-black py-2 px-6 m-2 rounded-lg text-xl font-medium"
+        >
+          Save
+        </button>
+        <button className="text-black  py-2 px-6 m-2 rounded-lg text-xl font-medium">
+          Cancel
+        </button>
+      </div>
+      <div>
+        <label htmlFor="projectName" className="">
+          Project Name
+        </label>
+        <input type="text" id="projectName" name="projectName" />
+        <label htmlFor="projectDescription" className="">
+          Project Description
+        </label>
+        <input type="text" id="projectDescription" name="projectDescription" />
+        <label htmlFor="projectDueDate" className="">
+          Project Due Date
+        </label>
+        <input type="date" id="projectDueDate" name="projectDueDate" />
+      </div>
     </form>
   );
 }
